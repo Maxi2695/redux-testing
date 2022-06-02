d0f773e4c2b200e8c39e0e80260ac735f8090af7
maksim.sukhov <maxi2695@yanex.ru>
2022-06-02 13:29:28 +0300

    lesson 12

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -4,7 +4,8 @@ import cartReducer, {
   addToCart,
   removeFromCart,
   getNumItems,
-  getTotalPrice
+  getTotalPrice,
+  checkoutCart,
 } from "./cartSlice";
 
 import { getMemoizedNumItems } from "./cartSlice";
@@ -274,6 +275,40 @@ describe("selectors", () => {
       result = getTotalPrice({ ...state });
       expect(result).toEqual("0.00");
       expect(getTotalPrice.recomputations()).toEqual(2);
+    });
+  });
+});
+
+describe("thunks", () => {
+  describe("checkoutCart w/mocked dispatch", () => {
+    it("should checkout", async () => {
+      const dispatch = jest.fn();
+      const state: RootState = {
+        products: { products: {} },
+        cart: { checkoutState: "READY", errorMessage: "", items: { abc: 123 } },
+      };
+      const thunk = checkoutCart();
+      await thunk(dispatch, () => state, undefined);
+      const { calls } = dispatch.mock;
+      expect(calls).toHaveLength(2);
+      expect(calls[0][0].type).toEqual("cart/checkout/pending");
+      expect(calls[1][0].type).toEqual("cart/checkout/fulfilled");
+      expect(calls[1][0].payload).toEqual({ success: true });
+    });
+    it("should fail with no items", async () => {
+      const dispatch = jest.fn();
+      const state: RootState = {
+        products: { products: {} },
+        cart: { checkoutState: "READY", errorMessage: "", items: {} },
+      };
+      const thunk = checkoutCart();
+      await thunk(dispatch, () => state, undefined);
+      const { calls } = dispatch.mock;
+      expect(calls).toHaveLength(2);
+      expect(calls[0][0].type).toEqual("cart/checkout/pending");
+      expect(calls[1][0].type).toEqual("cart/checkout/rejected");
+      expect(calls[1][0].payload).toEqual(undefined);
+      expect(calls[1][0].error.message).toEqual('Must include cart items');
     });
   });
 });
